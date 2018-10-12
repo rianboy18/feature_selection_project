61837aff28b6583505c918d7a6140b0f8772eeca
rianboy18 <pranavr95.pn@gmail.com>
2018-10-12 09:06:58 +0000

    Done

--- q04_select_from_model/build.py
+++ q04_select_from_model/build.py
@@ -1,3 +1,4 @@
+# %load q04_select_from_model/build.py
 # Default imports
 from sklearn.feature_selection import SelectFromModel
 from sklearn.ensemble import RandomForestClassifier
@@ -5,6 +6,19 @@ import pandas as pd
 import numpy as np
 
 data = pd.read_csv('data/house_prices_multivariate.csv')
+def select_from_model(data):
+    X=data.drop(['SalePrice'],axis=1)
+    y=data['SalePrice']
+    rfc = RandomForestClassifier()
+    sm = SelectFromModel(rfc)
+    model = sm.fit(X,y)
+    result=model.get_support()
+    res=[]
+    for i,j in zip(result,list(X)):
+        if i==True:
+            res.append(j)
+    return res
+c=select_from_model(data)
+c
 
 
-# Your solution code here
