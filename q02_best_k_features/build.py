9fcb756607e75c876e0d89a3f02e400636157338
rianboy18 <pranavr95.pn@gmail.com>
2018-10-12 08:55:54 +0000

    Done

--- q02_best_k_features/build.py
+++ q02_best_k_features/build.py
@@ -1,3 +1,4 @@
+# %load q02_best_k_features/build.py
 # Default imports
 
 import pandas as pd
@@ -8,5 +9,16 @@ from sklearn.feature_selection import SelectPercentile
 from sklearn.feature_selection import f_regression
 
 
-# Write your solution here:
+def percentile_k_features(data,k=20):
+    X=data.drop(['SalePrice'],axis=1)
+    y=data['SalePrice']
+    
+    SP=SelectPercentile(f_regression,percentile=k)
+    model=SP.fit_transform(X,y)
+    return model
+c=percentile_k_features(data,k=20)
+c
+
+
+
 
--- q03_rf_rfe/build.py
+++ q03_rf_rfe/build.py
@@ -1,3 +1,4 @@
+# %load q03_rf_rfe/build.py
 # Default imports
 import pandas as pd
 
@@ -6,6 +7,20 @@ data = pd.read_csv('data/house_prices_multivariate.csv')
 from sklearn.feature_selection import RFE
 from sklearn.ensemble import RandomForestClassifier
 
+def rf_rfe(data):
+    X = data.drop(['SalePrice'],axis=1)
+    y = data['SalePrice']
+    model = RandomForestClassifier()
+    rfe = RFE(model)
+    rfe.fit(X,y)
+    res=[]
+    for i,j in zip((rfe.ranking_),(list(X))):
+        if i==1:
+            res.append(j)
+    return res
 
-# Your solution code here
+
+c=rf_rfe(data)
+c
+
 
